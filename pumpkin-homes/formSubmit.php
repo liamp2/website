8c5ad0e1f14f6b8c87493cb860f69860b4e26644
lpatterson <lpatterson@extremenetworks.com>
2021-06-24 12:28:23 -0400

    decodes data and formats form data for simple email

--- pumpkin-homes/formSubmit.php
+++ pumpkin-homes/formSubmit.php
@@ -1,8 +1,14 @@
 <?php
-    // if($_POST["message"]) {
-
-    $name = $_POST['applicantName'];
-    echo $name;
+  
+  
+    $data = $_POST['data'];
+    $emailBody = "";
+    $decoded = json_decode($data, true);
+    foreach($decoded as $key => $value){
+        $emailBody = $emailBody . $key. ": " .  $value. "<br>";
+    }
+    echo $emailBody;
+    //echo $phoneNumber;
     // if(mail("liamhockey2@gmail.com","Here is the subject line", "my message")){
     //     echo "Success";
     // } else{
