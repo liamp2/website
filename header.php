f2447342bfb54417487e7431cf6030ecfb90ad84
liampatterson02@gmail.com <liampatterson02@gmail.com>
2021-06-24 23:32:00 -0400

    changed to use php files

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+    <title> Liam Patterson  </title>
+    <link rel="stylesheet" href="main.css">
+    
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/ef72bf579d.js" crossorigin="anonymous"></script>
+
+   <style>
+        @media screen and (max-width:450px){
+            .topNav a:not(:first-child) {display: none; }
+            .topNav a.icon{
+                float: right;
+                display: block;
+            }
+        }
+
+        @media screen and (max-width:450px){
+            .topNav.responsive {position: sticky; }
+            .topNav.responsive .icon{
+                position: sticky;
+                right: 0;
+                top: 0;
+            }
+            .topNav.responsive a{
+                float: none;
+                display: block;
+                text-align: left;
+            }
+        }
+        
+    </style>
+</head>
