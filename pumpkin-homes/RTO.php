93bb25ac34d52b3785182c65825e9d508011e0e3
liampatterson02@gmail.com <liampatterson02@gmail.com>
2021-06-24 22:17:29 -0400

    added expanding and hiding all FAQ at a time

--- pumpkin-homes/RTO.php
+++ pumpkin-homes/RTO.php
@@ -131,6 +131,7 @@
         <p class="text-center text-muted">
             ​While we cannot anticipate all questions, below are answers to the most common questions you may have about RTO program. We are happy to provide a further explanation on any or all of the questions below. It is important to remember we are a small private company and deal with most situations on a case by case basis as there is never a 'one shoe fits all' solution in this business. Trust that we are not a rigid and bureaucratic bank. We are here to share our experience and help you realize your dream of home ownership.
         </p>
+        <button id="expandFAQbtn" data-bs-toggle="collapse" data-bs-target=".multi-collapse" class="btn btn-primary text-white my-3">Expand All</button>
         <div class="row">
             <div class="col-md-6 d-flex flex-column" id="accordion1">
                 <div class="card order-1">
@@ -139,7 +140,7 @@
                             How much of an initial deposit is required?
                         </h5>
                     </a>
-                    <div id="collapse1" class="collapse">
+                    <div id="collapse1" class="collapse multi-collapse">
                         <div class="card-body">
                             A minimum deposit of $8,000 or 3-4% of the initial purchase price will be required. This initial deposit will be credited 100% towards the  final down payment for your home.
                         </div>
@@ -152,7 +153,7 @@
                             How is my monthly rent set and how is it applied to the eventual down payment?
                         </h5>
                     </a>
-                    <div id="collapse2" class="collapse">
+                    <div id="collapse2" class="collapse multi-collapse">
                         <div class="card-body">
                             Your fixed monthly payment includes the following items: rent, property taxes, property insurance and a monthly down payment credit towards the final down payment. The monthly down payment credit is normally 20% of the total rent and is an amount always identified in the contract.
                         </div>
@@ -165,7 +166,7 @@
                             How much will I save for the final down payment?
                         </h5>
                     </a>
-                    <div id="collapse3" class="collapse">
+                    <div id="collapse3" class="collapse multi-collapse">
                         <div class="card-body">
                             The amount saved for the final down payment is the sum total of the initial deposit plus all the monthly down payment credits.  The goal is to have 8-10% of the purchase price saved as a final down payment.  As an example:  if the initial purchase price was $270K then a 4% deposit would be about $11K,  the final guaranteed purchase price after 3 years would be about $300K.  The monthly savings towards your down payment credit would be  $444 per month x 36 months=$16K.   Thus your total savings towards the final down payment would be $27K ($11K + $16K) or about 9% of the final purchase price. We strongly encourage clients to accumulate a 10% down payment.
                         </div>
@@ -178,7 +179,7 @@
                             What is the typical rental period?
                         </h5>
                     </a>
-                    <div id="collapse4" class="collapse">
+                    <div id="collapse4" class="collapse multi-collapse">
                         <div class="card-body">
                             The typical rental period is 3-4 years. The term is based on how long it will take to repair or establish your credit, and the time it will take to save for your final down payment.
                         </div>
@@ -191,7 +192,7 @@
                             Can I make changes to the house and property during the rental period?
                         </h5>
                     </a>
-                    <div id="collapse5" class="collapse">
+                    <div id="collapse5" class="collapse multi-collapse">
                         <div class="card-body">
                             Yes. This is one of the many advantages of RTO program. You can paint, install laminate flooring, landscape, build a deck, install a jungle gym in the backyard and treat the house like your own. Any large or structural changes we will want to review and approve, but as long as it increases the overall value and appeal of the home, you will get the green light.  If a building permit is required this will be your responsibility.
                         </div>
@@ -204,7 +205,7 @@
                             Could you help us stay in our existing home versus having to sell it to clear debts?
                         </h5>
                     </a>
-                    <div id="collapse6" class="collapse">
+                    <div id="collapse6" class="collapse multi-collapse">
                         <div class="card-body">
                             Yes – we call this our Home Refinance Option.  The RTO program can be used to help you stay in your existing home, your existing neighbourhood, and keep your kids at the same school. The same process for qualification and approval will apply. We agree on a current purchase price and then confirm the value with a home appraisal.   When closing the purchase we provide your lawyer with 93-95% of the agreed price to pay off your existing mortgage and debts.  The remaining 5-7% is treated as an initial deposit for the final down payment.  We have helped several families with our Home Refinance Option and keep their homes.
                         </div>
@@ -217,7 +218,7 @@
                             Why do you focus in Barrie, Simcoe County and northern York Region?
                         </h5>
                     </a>
-                    <div id="collapse7" class="collapse">
+                    <div id="collapse7" class="collapse multi-collapse">
                         <div class="card-body">
                             We prefer to meet all clients personally and remain involved during the rental period. We therefore prefer to help families in the communities near our home in Aurora. We are also better equipped to lend real estate experience in a market that is familiar to us.
                             <br>
@@ -238,7 +239,7 @@
                             What personal information is needed and who will this be shared with?
                         </h5>
                     </a>
-                    <div id="collapse8" class="collapse">
+                    <div id="collapse8" class="collapse multi-collapse">
                         <div class="card-body">
                             For the finance approval stage the same standard information requested for any loan will be required: two pieces of identification, 2 years of T4’s, 2 years of tax returns (NOA), full credit report, and an employment letter. This information will only be reviewed by a Pumpkin Home owner, our mortgage broker, or joint venture partner. It will only be shared with your approval.
                         </div>
@@ -251,7 +252,7 @@
                             Who pays for the home inspection and appraisal?
                         </h5>
                     </a>
-                    <div id="collapse9" class="collapse">
+                    <div id="collapse9" class="collapse multi-collapse">
                         <div class="card-body">
                             We initially pay for the home inspection and appraisal, and you would reimburse us within the first 60-90 days, since after all this will be your home.  The home inspection is crucial since you will want to know as much as possible about its condition before we decide to make an offer. We will both review the results of the home inspection and make a mutual decision on how to proceed if there are any significant issues identified.
                         </div>
@@ -264,7 +265,7 @@
                             Who pays for property taxes?
                         </h5>
                     </a>
-                    <div id="collapse10" class="collapse">
+                    <div id="collapse10" class="collapse multi-collapse">
                         <div class="card-body">
                             Pumpkin Homes will pay the property taxes during the rental period.
                         </div>
@@ -277,7 +278,7 @@
                             Who pays for utilities and general upkeep?
                         </h5>
                     </a>
-                    <div id="collapse11" class="collapse">
+                    <div id="collapse11" class="collapse multi-collapse">
                         <div class="card-body">
                             You would pay for items just like any home owner. Examples of these normal costs are ALL utilities including hydro, water, gas, cable, phone, internet, and property maintenance like lawncare, leaf raking and snow removal.
                         </div>
@@ -290,7 +291,7 @@
                             Who pays for maintenance and large repairs?
                         </h5>
                     </a>
-                    <div id="collapse12" class="collapse">
+                    <div id="collapse12" class="collapse multi-collapse">
                         <div class="card-body">
                             You would pay for standard maintenance items just like any home owner. You would be responsible for standard maintenance items with costs up to $1,000 per occurrence (leaky toilet, broken dishwasher, broken window, etc) up to an annual value of $5,000.  We will pay for any large expenses (broken air conditioner, roof) and then reach an agreement with you to be reimbursed over time, or at the end of term.
                         </div>
@@ -303,7 +304,7 @@
                             Who pays for house insurance?
                         </h5>
                     </a>
-                    <div id="collapse13" class="collapse">
+                    <div id="collapse13" class="collapse multi-collapse">
                         <div class="card-body">
                             This is actually a shared expense. Pumpkin Homes will pay for standard house insurance to cover the house, property and liability. During the rental period you will need to take out tenant insurance to cover your personal belongings in the house. Proof of tenant insurance will be required before you move in.
                         </div>
@@ -316,7 +317,7 @@
                             How do I improve my credit rating during the rental term?
                         </h5>
                     </a>
-                    <div id="collapse14" class="collapse">
+                    <div id="collapse14" class="collapse multi-collapse">
                         <div class="card-body">
                             We will discuss before entering into an agreement if your credit rating needs to improve before the end of the rental term.  If improvement is required, we will need you to meet regularly (every 6 months) with a credit repair specialist of your choice to seek professional advice, and to provide us regular updates on the progress.  This is important to ensure you have the best possible chance of qualifying for your own mortgage, at favourable rates, at the end of term.
                         </div>
@@ -329,7 +330,7 @@
                             What happens if I change my mind?
                         </h5>
                     </a>
-                    <div id="collapse15" class="collapse">
+                    <div id="collapse15" class="collapse multi-collapse">
                         <div class="card-body">
                             We have all committed a lot of time, energy and financial resources to helping you achieve your dream of  buying a home so we really hope you do not change your mind. If you do, yes you can walk away at the end of the rental term, however you would lose your initial deposit as well as most of your monthly down  payment credits, as defined in the contract.
                         </div>
@@ -342,6 +343,12 @@
 </section>
 
 <?php include 'footer.php' ?>
+<script>
+    $('#expandFAQbtn').click(function(){
+        var el = $('#expandFAQbtn');
+        el.text(el.text() == 'Expand All' ? 'Hide All' : 'Expand All');           
+    });
+</script>
 
 </body>
 </html>
