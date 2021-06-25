80fef1d38fca9435254ea0b13d7f5ee896ef9a92
liampatterson02@gmail.com <liampatterson02@gmail.com>
2021-06-24 20:04:39 -0400

    added active page css and sticky top

--- pumpkin-homes/navbar.php
+++ pumpkin-homes/navbar.php
@@ -1,5 +1,5 @@
 <!-- NAVBAR -->
-<nav class="navbar navbar-expand-md navbar-light bg-light">
+<nav id="navbar" class="navbar navbar-expand-md navbar-light bg-light">
     <div class="container">
         <a href="index.php" class="navbar-brand">Pumpkin Homes</a>
         <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -8,26 +8,33 @@
 
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item active">
-                    <a href="index.php" class="nav-link">Home</a>
+                <li class="nav-item <?php if($activePage=="index.php"){echo "active";}?>">
+                    <a href="index.php" class="nav-link <?php if($activePage=="index.php"){echo "active";}?>">Home</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a href="feature-home.php" class="nav-link">Feature Home</a>
+                </li> -->
+                <li class="nav-item <?php if($activePage=="RTO.php"){echo "active";}?>">
+                    <a href="RTO.php" class="nav-link <?php if($activePage=="RTO.php"){echo "active";}?>">RTO Program</a>
                 </li>
-                <li class="nav-item">
-                    <a href="RTO.php" class="nav-link">RTO Program</a>
-                </li>
-                <li class="nav-item">
-                    <a href="about.php" class="nav-link">About Us</a>
+                <li class="nav-item <?php if($activePage=="about.php"){echo "active";}?>">
+                    <a href="about.php" class="nav-link <?php if($activePage=="about.php"){echo "active";}?>">About Us</a>
                 </li>
                 <li class="nav-item">
                     <!-- <a href="apply.php" class="nav-link">Apply Now</a> -->
-                    <a href="" class="nav-link" data-bs-target="#application-modal" data-bs-toggle="modal">Apply Now</a>
+                    <a class="nav-link" data-bs-target="#application-modal" data-bs-toggle="modal">Apply Now</a>
                 </li>
-                <li class="nav-item">
-                    <a href="partner.php" class="nav-link">Partners</a>
+                <li class="nav-item <?php if($activePage=="partner.php"){echo "active";}?>">
+                    <a href="partner.php" class="nav-link <?php if($activePage=="partner.php"){echo "active";}?>">Partners</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+<script>
+    window.onscroll = function(e) {
+    // print "false" if direction is down and "true" if up
+        (this.oldScroll > this.scrollY) ? $('#navbar').addClass('sticky-top') : $('#navbar').removeClass('sticky-top');
+        this.oldScroll = this.scrollY;
+    }
+</script>
